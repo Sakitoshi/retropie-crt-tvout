c07189136a2055f99369790f980804fddfd69408
Sakitoshi <39132563+Sakitoshi@users.noreply.github.com>
2018-07-21 01:58:10 -0400

    Update runcommand-onstart.sh
    added a function to allow forcing a whole system to 480i

--- to_configs/all/runcommand-onstart.sh
+++ to_configs/all/runcommand-onstart.sh
@@ -1,2 +1,2 @@
 if [ -s "/opt/retropie/configs/$1/480i.txt" ]; then interlaced=$(tr -d "\r" < "/opt/retropie/configs/$1/480i.txt"); else interlaced="none"; fi > /dev/null
-if [[ "tvservice -s | grep NTSC" ]] && ! echo "$3" | grep -i "$interlaced"; then tvservice -c "NTSC 4:3 P"; fbset -depth 8; fbset -depth 32; fi > /dev/null
+if tvservice -s | grep NTSC && ! echo "$3" | grep -wi "$interlaced" && ! echo "$interlaced" | grep -wi "all"; then tvservice -c "NTSC 4:3 P"; fbset -depth 8; fbset -depth 32; fi > /dev/null
