faaa6610d9950c9e75634db0e8857ed66439ddf8
Sakitoshi <39132563+Sakitoshi@users.noreply.github.com>
2018-07-21 23:11:12 -0400

    check if interlaced variable is empty
    this helps determine if the whole system should be forced into 480i mode or not, previously it would activate 480i mode if a 480i.txt file containing only spaces or enters existed.

--- to_configs/all/runcommand-onstart.sh
+++ to_configs/all/runcommand-onstart.sh
@@ -1,2 +1,3 @@
-if [ -s "/opt/retropie/configs/$1/480i.txt" ]; then interlaced=$(tr -d "\r" < "/opt/retropie/configs/$1/480i.txt"); else interlaced="none"; fi > /dev/null
+interlaced=$(tr -d "\r" < "/opt/retropie/configs/$1/480i.txt")
+if [ ! -s "/opt/retropie/configs/$1/480i.txt" ] || [ -z "$interlaced" ]; then interlaced="none"; fi > /dev/null
 if tvservice -s | grep NTSC && ! echo "$3" | grep -wi "$interlaced" && ! echo "$interlaced" | grep -wi "all"; then tvservice -c "NTSC 4:3 P"; fbset -depth 8; fbset -depth 32; fi > /dev/null
