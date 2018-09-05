937f9353c4fbef761d2c024febf4e4660c3cfa82
Sakitoshi <sakitoshi@gmail.com>
2018-09-05 15:09:01 -0300

    little correction
    it shouldn't make a big change in the behavior of the 480i.txt file, it's more a safety check than anything.

--- to_configs/all/runcommand-onstart.sh
+++ to_configs/all/runcommand-onstart.sh
@@ -1,4 +1,4 @@
 if [ -f "/opt/retropie/configs/$1/480i.txt" ]; then interlaced=$(tr -d "\r" < "/opt/retropie/configs/$1/480i.txt"); fi > /dev/null
 if [ -f "/opt/retropie/configs/ports/$1/480i.txt" ]; then interlaced=$(tr -d "\r" < "/opt/retropie/configs/ports/$1/480i.txt"); fi > /dev/null
-if [ ! -s "/opt/retropie/configs/$1/480i.txt" && ! -s "/opt/retropie/configs/$1/480i.txt" ] || [ -z "$interlaced" ]; then interlaced="none"; fi > /dev/null
+if [ ! -s "/opt/retropie/configs/$1/480i.txt" && ! -s "/opt/retropie/configs/ports/$1/480i.txt" ] || [ -z "$interlaced" ]; then interlaced="none"; fi > /dev/null
 if tvservice -s | grep NTSC && ! echo "$3" | grep -wi "$interlaced" && ! echo "$interlaced" | grep -wi "all"; then tvservice -c "NTSC 4:3 P"; fbset -depth 8; fbset -depth 32; fi > /dev/null
